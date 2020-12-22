0e476ba5e8406e772f6200c7c72d0a6d7fea0887
Simon Lambin <slambin@artfx.fr>
2020-12-22 08:49:26 +0100

    feat(lSystemWord class): Add iterator

--- source/l-SystemInterpreter/public/l-SystemInterpreter/l-SystemWord.h
+++ source/l-SystemInterpreter/public/l-SystemInterpreter/l-SystemWord.h
@@ -4,6 +4,41 @@
 #include <string>
 #include <vector>
 
+class LSystemWordIterator
+{
+public:
+    // Constructors / desctructors
+    LSystemWordIterator(LSystemModule* _lSystemModule) : lSystemModule(_lSystemModule) { }
+    
+    // Operators
+    LSystemWordIterator& operator++()
+    {
+        lSystemModule++;
+        return *this;
+    }
+    LSystemWordIterator operator++(int)
+    {
+        LSystemWordIterator _iterator = *this;
+        lSystemModule++;
+        return _iterator;
+    }
+    LSystemWordIterator& operator--()
+    {
+        lSystemModule--;
+        return *this;
+    }
+    LSystemWordIterator operator--(int)
+    {
+        LSystemWordIterator _iterator = *this;
+        lSystemModule--;
+        return _iterator;
+    }
+    
+
+private:
+    LSystemModule* lSystemModule; 
+};
+
 // Array of elements representing the result or a step of an l-system
 class LSystemWord
 {
@@ -15,6 +50,9 @@ public:
         COMMAND
     };
 
+    // Aliases
+    using Iterator = LSystemWordIterator;
+
     // Constructors / desctructors
     LSystemWord(std::string _word);
     LSystemWord(LSystemWord& _lSystemWord);
@@ -23,6 +61,14 @@ public:
     // Getters / setters / append
     void appendModule(LSystemModule _module);
     std::vector<LSystemModule> getModules() { return modules; }
+
+    // Iterator
+    Iterator begin() { return Iterator(&(modules[0])); }
+    Iterator end() 
+    { 
+        LSystemModule* lastModule = &modules[modules.size()-1];
+        return Iterator(++lastModule); 
+    }
 
 private:
     // Private functions
